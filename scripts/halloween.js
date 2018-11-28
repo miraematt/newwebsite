f5c461a1df4867b288bd2fcdca25ec955758e52b
miraematt <matttootell@hotmail.com>
2018-11-28 23:51:45 +0900

    add mirae and project page

---
+++ scripts/halloween.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+	$("#slideshow1 > div:gt(0)").hide();
+
+	setInterval(function() { 
+  		$('#slideshow1 > div:first')
+		    .fadeOut(1000)
+		    .next()
+		    .fadeIn(1000)
+		    .end()
+		    .appendTo('#slideshow1');
+		},  5000);
+   
+   $("#slideshow2 > div:gt(0)").hide();
+
+	setInterval(function() { 
+  		$('#slideshow2 > div:first')
+		    .fadeOut(1000)
+		    .next()
+		    .fadeIn(1000)
+		    .end()
+		    .appendTo('#slideshow2');
+		},  5000);
+   
+   $("#slideshow3 > div:gt(0)").hide();
+
+	setInterval(function() { 
+  		$('#slideshow3 > div:first')
+		    .fadeOut(1000)
+		    .next()
+		    .fadeIn(1000)
+		    .end()
+		    .appendTo('#slideshow3');
+		},  5000);
+   
+});
